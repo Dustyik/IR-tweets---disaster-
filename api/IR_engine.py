9774e16239f7b46fc1e819fd36cbaba843a892c3
Eugene <eugenetdr@gmail.com>
2021-07-20 16:08:46 +0800

    updated engine file

--- api/IR_engine.py
+++ api/IR_engine.py
@@ -26,14 +26,13 @@ class DataProcessor:
 		self.data['clean_text'] = self.data.apply(lambda x: self.tokenize_stem_lower(x.text), axis=1)
 		return self.data 
 
-class DocumentTerm:
+class CosineSimilarity:
 
-	def __init__(self, data):
+	def __init__(self, data, type='tfidf'):
+		self.data = data 
+		self.change_matrix_type(type)
 
-		self.vec = CountVectorizer()
-		self.data = data
-
-	def cos_sim(self, return_size):
+	def get_result(self, return_size):
 		cos_sim = cosine_similarity(self.matrix, self.matrix)
 		top_ind = np.flip(np.argsort(cos_sim[0]))[1:return_size+1]
 		top_id = [list(self.matrix.index)[i] for i in top_ind]
@@ -52,70 +51,33 @@ class DocumentTerm:
 		            score = -1
 		        self.result.append({'tweet_id':i, 'text': text, 'related_article':related,'score': score})
 
-	def euclidean(self, return_size):
-		euclidean = euclidean_distances(self.matrix.values[1:], [self.matrix.values[0]])
-		top_ind = np.argsort(euclidean.T[0])[:return_size]
-		top_id = [list(self.matrix.index)[i] for i in top_ind]
-		# print(sorted(euclidean[:20]),top_10_ind ,top_10_id)
-		self.result = []
-		for i in top_id:
-		    filt = self.data[self.data.document==i]
-		    for ind, r in filt.iterrows():
-		        rel = r['rel']
-		        text = r['text']
-		        related = r['topic']
-		        score = 0
-		        if related==self.query_id and rel>0:
-		            score = 1
-		        if related==self.query_id and rel==0:
-		            score = -1
-		        self.result.append({'tweet_id':i, 'text': text, 'related_article':related,'score': score})
-
-	def query(self, query_id, query_text, method, return_size=10):
+	def query(self, query_id, query_text, return_size=10):
 		self.query_id = query_id
 		term_doc = self.vec.fit_transform([query_text]+list(self.data.clean_text))
 		ind = ['query'] + list(self.data.document)
 		self.matrix = pd.DataFrame(term_doc.toarray(), columns=self.vec.get_feature_names(), index=ind)
-
-		if method == 'cos_sim':
-			self.cos_sim(return_size)
-		elif method == 'euclidean':
-			self.euclidean(return_size)
-		else: 
-			print("Method is not valid.")
-
+		self.get_result(return_size)
 		return pd.DataFrame(self.result)
+
+	def change_matrix_type(self, type):
+		if type == 'tfidf':
+			self.vec = TfidfVectorizer() 
+		elif type == 'dt':
+			self.vec = CountVectorizer()
+		else:
+			print('Type is invalid')
 
 	def get_matrix(self):
 		return self.matrix
 
-class TfIdf:
+class EuclideanDistance:
 
-	def __init__(self, data):
+	def __init__(self, data, type='tfidf'):
+		self.data = data 
+		self.change_matrix_type(type)
+		self.matrix = None
 
-		self.vec = TfidfVectorizer()
-		self.data = data
-
-	def cos_sim(self, return_size):
-		cos_sim = cosine_similarity(self.matrix, self.matrix)
-		top_ind = np.flip(np.argsort(cos_sim[0]))[1:return_size+1]
-		top_id = [list(self.matrix.index)[i] for i in top_ind]
-		# print(top_10_ind ,top_10_id)
-		self.result = []
-		for i in top_id:
-		    filt = self.data[self.data.document==i]
-		    for ind, r in filt.iterrows():
-		        rel = r['rel']
-		        text = r['text']
-		        related = r['topic']
-		        score = 0
-		        if related==self.query_id and rel>0:
-		            score = 1
-		        if related==self.query_id and rel==0:
-		            score = -1
-		        self.result.append({'tweet_id':i, 'text': text, 'related_article':related,'score': score})
-
-	def euclidean(self, return_size):
+	def get_result(self, return_size):
 		euclidean = euclidean_distances(self.matrix.values[1:], [self.matrix.values[0]])
 		top_ind = np.argsort(euclidean.T[0])[:return_size]
 		top_id = [list(self.matrix.index)[i] for i in top_ind]
@@ -134,20 +96,21 @@ class TfIdf:
 		            score = -1
 		        self.result.append({'tweet_id':i, 'text': text, 'related_article':related,'score': score})
 
-	def query(self, query_id, query_text, method, return_size=10):
+	def query(self, query_id, query_text, return_size=10):
 		self.query_id = query_id
 		term_doc = self.vec.fit_transform([query_text]+list(self.data.clean_text))
 		ind = ['query'] + list(self.data.document)
 		self.matrix = pd.DataFrame(term_doc.toarray(), columns=self.vec.get_feature_names(), index=ind)
-
-		if method == 'cos_sim':
-			self.cos_sim(return_size)
-		elif method == 'euclidean':
-			self.euclidean(return_size)
-		else: 
-			print("Method is not valid.")
-
+		self.get_result(return_size)
 		return pd.DataFrame(self.result)
+
+	def change_matrix_type(self, type):
+		if type == 'tfidf':
+			self.vec = TfidfVectorizer() 
+		elif type == 'dt':
+			self.vec = CountVectorizer()
+		else:
+			print('Type is invalid')
 
 	def get_matrix(self):
 		return self.matrix
